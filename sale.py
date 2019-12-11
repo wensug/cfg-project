31d5ff24fc7ead4754d9794cb0730312f517e8dc
wensug <wensug@gmail.com>
2019-12-11 19:35:07 +0000

    The average and highest

--- sale.py
+++ sale.py
@@ -1,7 +1,16 @@
 import csv
-def calculate_percentage(sales1):  
-    for a, b in zip(sales1[::1], sales1[1::1]):
-        return 100 * (b - a) / a
+from statistics import mean 
+def calculate_percentage(sales):  
+    # for a, b in zip(sales1[::1], sales1[1::1]):
+    #     return 100 * (b - a) / a 
+    return [100 * (b - a) / a for a, b in zip(sales[::1], sales[1::1])]    
+
+def average(sales): 
+    return mean(sales) 
+
+def highest_sales(sales):
+    return max(sales)
+
 
 with open('sales.csv', 'r') as sale_file:
     spreadsheet = csv.DictReader(sale_file)
@@ -12,5 +21,10 @@ with open('sales.csv', 'r') as sale_file:
         print(dict(row))
 total_sales = sum(sales_month)
 percentaje_month = calculate_percentage(sales_month)
-print(percentaje_month)
-print(total_sales)
+roundpercentage=['%.2f' % elem for elem in percentaje_month]
+average_month = average(sales_month)
+highest_month = highest_sales(sales_month)
+print('highest month: {}'.format(highest_month))
+print('The average: {}'.format(average_month))
+print('Monthly percentage change: {}' .format(roundpercentage))
+print('Total sales: {}'.format(total_sales))
